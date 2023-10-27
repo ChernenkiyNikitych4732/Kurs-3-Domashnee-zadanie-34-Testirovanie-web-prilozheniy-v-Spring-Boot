7963280881eef06a13ebd259f6ec73aa53f5f2e0
Nikita Chernenkiy <chernenkiy.nikitych136@yandex.ru>
2023-10-27 14:04:50 +0200

    Курс 3 Домашнее задание 34 Тестирование веб приложений в Spring Boot

--- src/main/java/sky/pro/course3/homework/HogwartsApplication.java
+++ src/main/java/sky/pro/course3/homework/HogwartsApplication.java
@@ -1,13 +1,15 @@
 package sky.pro.course3.homework;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Course3Homework34TestWebAppOnSpringBoot2Application {
+@OpenAPIDefinition
+public class HogwartsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Course3Homework34TestWebAppOnSpringBoot2Application.class, args);
+        SpringApplication.run(HogwartsApplication.class, args);
     }
 
 }
